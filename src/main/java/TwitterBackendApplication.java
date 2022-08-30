9ba3404123e1340d5f0a40009ae51a7f3df6af20
JRan-37 <79283020+JRan-37@users.noreply.github.com>
2022-08-30 14:59:08 -0400

    add initial files

---
+++ src/main/java/TwitterBackendApplication.java
@@ -0,0 +1,10 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterBackendApplication.class, args);
+    }
+}
