053e63c2679e99ba8eaea5f0ee956e9c1ba39501
JRan-37 <79283020+JRan-37@users.noreply.github.com>
2022-09-06 01:02:40 -0400

    add tweet and user repo implementations

--- src/main/java/services/TwitterService.java
+++ src/main/java/services/TwitterService.java
@@ -4,10 +4,12 @@ import com.twitter.clientlib.TwitterCredentialsBearer;
 import com.twitter.clientlib.api.TwitterApi;
 import com.twitter.clientlib.model.Tweet;
 import com.twitter.clientlib.model.User;
-import models.UserTweet;
+import entities.UserTweet;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
+import repositories.TweetRepository;
+import repositories.UserRepository;
 
 import java.util.List;
 
@@ -17,17 +19,42 @@ public class TwitterService {
     private static final String _bearerToken = "";
     private static final Logger log = LogManager.getLogger(TwitterService.class);
 
-    //returns a list of tweets from a given twitter user
-    public List<UserTweet> getTweets(String userID) {
+    TweetRepository tweetRepository;
+    UserRepository userRepository;
+
+    public TwitterService(TweetRepository tweetRepository, UserRepository userRepository)
+    {
+        this.tweetRepository = tweetRepository;
+        this.userRepository = userRepository;
+    }
+
+    //adds a list of tweets from a given twitter user to the database and returns the list
+    public List<UserTweet> addTweets(String userID) {
         //Initiate twitter api with dev platform bearer token
         TwitterApi api = new TwitterApi(new TwitterCredentialsBearer(_bearerToken));
         //Find the twitter user based on supplied userID
         User twitterUser = getTwitterUserByID(api, userID);
+        //TODO Convert from twitter's user model to our backend model
+        //UserProfile userProfile = createUser(twitterUser);
+
         //Store a list of the user's tweets
         List<Tweet> twitterUserTweets = getUserTweets(api, twitterUser);
         //Convert from twitter's tweet model to our backend model
         List<UserTweet> tweetList = createTweetList(twitterUserTweets);
 
+        //TODO Save user to database
+        //userRepository.save(userProfile);
+
+        //Save tweets to database
+        for(UserTweet tweet : tweetList)
+            tweetRepository.save(tweet);
+
+        return tweetList;
+    }
+
+    public List<UserTweet> getTweets(String userID) {
+        List<UserTweet> tweetList = null;
+        //TODO return tweets from database
         return tweetList;
     }
 
