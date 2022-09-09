9648c79581c25a960b71bb560b43e87758676fd0
JRan-37 <79283020+JRan-37@users.noreply.github.com>
2022-09-09 15:30:54 -0400

    add websecurity implementation

--- src/main/java/twitterbackend/security/WebSecurityConfig.java
+++ src/main/java/twitterbackend/security/WebSecurityConfig.java
@@ -1,4 +1,80 @@
 package twitterbackend.security;
 
-public class WebSecurityConfig {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import twitterbackend.jwt.JwtAuthenticationEntryPoint;
+import twitterbackend.jwt.JwtRequestFilter;
+import twitterbackend.services.ClientUserService;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    ClientUserService clientUserService;
+    JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    JwtRequestFilter jwtRequestFilter;
+
+    public WebSecurityConfig (ClientUserService clientUserService,
+                              JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
+                              JwtRequestFilter jwtRequestFilter) {
+
+        this.clientUserService = clientUserService;
+        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+        this.jwtRequestFilter = jwtRequestFilter;
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        // configure AuthenticationManager so that it knows from where to load
+        // user for matching credentials
+        // Use BCryptPasswordEncoder
+        auth.userDetailsService(clientUserService).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable();
+
+        http
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http
+                .exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint);
+
+        http
+                .authorizeRequests()
+                .antMatchers("/tweets/by-user/{id}", "/tweets/by-user", "/tweets/by-user/**").permitAll()
+                .anyRequest().authenticated();
+
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
